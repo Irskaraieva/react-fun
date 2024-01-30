df93ab11377cc209e9fbe76334021aea89604ee2
Irskaraieva <mishytkina14@gmail.com>
2024-01-30 19:13:13 +0100

    change foter-link-style

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
         <>
             <footer className="hero-foot">
                 <nav className="tabs">
-                    <div className="container has-text-centered py-3 mb-1">
-                        <Link to={'https://github.com/Irskaraieva'} target="_blank" className="login-link has-text-grey-dark">Created by me</Link>
+                    <div className="container">
+                        <Link to={'https://github.com/Irskaraieva'} target="_blank" className="footer-link">Created by me</Link>
                     </div>
                 </nav>
             </footer>
